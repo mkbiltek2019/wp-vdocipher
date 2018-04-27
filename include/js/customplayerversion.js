43b771c5b423680e53a0a67e3f69413d45b486d7
Milan Gupta <milan.gupta.iitd@gmail.com>
2018-04-27 16:04:48 +0530

    HTML5 watermark 4th commit

--- include/js/customplayerversion.js
+++ include/js/customplayerversion.js
@@ -1,7 +1,7 @@
 var vdoPlayerVersion = document.getElementById('vdo_embed_version');
 var vdoCustomVersion = document.getElementById('vdo_custom_version')
 
-if (inarray(vdoVD.vdoSV, vdoVD.vdoAV)) hideVdoCustomVersion();
+if (inArray(vdoVD.vdoSV, vdoVD.vdoAV)) hideVdoCustomVersion();
 else showVdoCustomVersion(vdoVD.vdoSV);
 
 vdoPlayerVersion.onchange = function() {
@@ -15,12 +15,12 @@ function hideVdoCustomVersion() {
 }
 
 function showVdoCustomVersion(str) {
-  vdoPlayerVersion.value = 'Custom Version';
   vdoCustomVersion.style.display = 'inline-block';
+  vdoPlayerVersion.value = 'Custom Version';
   vdoCustomVersion.value = str;
 }
 
-function inarray(vdoVersion, vdoAvailable){
+function inArray(vdoVersion, vdoAvailable){
   for (var i = 0; i < vdoAvailable.length; i++){
     if (vdoVersion == vdoAvailable[i]) return true;
   }
