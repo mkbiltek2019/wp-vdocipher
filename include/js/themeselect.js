f30278e9a65cd09d7a0d7a7d3440b25010e0097e
Milan Gupta <milan.gupta.iitd@gmail.com>
2018-10-09 19:58:33 +0530

    Some refactoring. Done here

--- include/js/themeselect.js
+++ include/js/themeselect.js
@@ -1,7 +1,6 @@
 (function(){
 
 var vdoTheme = document.getElementById('vdo_player_theme_options');
-var vdoAvailableThemes = document.getElementById('available_themes');
 
 var vdoThemeImage = [
   document.getElementById('default1-image'),
@@ -27,14 +26,19 @@ var vdoThemeVal = [
   document.getElementById('default3-id').innerHTML,
   document.getElementById('color1-id').innerHTML,
   document.getElementById('color2-id').innerHTML,
-  document.getElementById('color3-id').innerHTML
+  document.getElementById('color3-id').innerHTML,
 ];
+
+var vdoColors = [
+'#0294f9',
+'#CFC10A',
+'#67AF17',
+]
 
 var vdoThemeNumber = vdoThemeVal.length;
 
 window.onload = function(){
-  var index = vdoThemeVal.indexOf(vdoTheme.value);
-  vdoThemeSelect(index);
+  vdoThemeSelect(vdoThemeVal.indexOf(vdoTheme.value));
 }
 
 for (var k = 0; k < vdoThemeNumber; k++){
@@ -42,14 +46,24 @@ for (var k = 0; k < vdoThemeNumber; k++){
   vdoThemeBtn[k].addEventListener('click', vdoThemeBtnClick);
 }
 
+function vdoThemeImageClick(e){
+  vdoThemeSelect(vdoThemeImage.indexOf(e.target));
+}
+
+function vdoThemeBtnClick(e){
+  vdoThemeSelect(vdoThemeBtn.indexOf(e.target));
+}
+
 function vdoThemeSelect(index){
   for (var j = 0; j < vdoThemeNumber; j++) {
     if (j===index){
       vdoTheme.value = vdoThemeVal[j];
-      vdoThemeImage[j].style.border="5px solid #00aaFF";
-      vdoThemeImage[j].style.width="97%";
+      vdoThemeImage[j].style.borderWidth="10px";
+      vdoThemeImage[j].style.borderStyle="solid";
+      vdoThemeImage[j].style.borderColor=vdoColors[j%3];
+      vdoThemeImage[j].style.width="92%";
       vdoThemeBtn[j].innerHTML = "Selected";
-      vdoThemeBtn[j].style.backgroundColor = "#0f0";
+      vdoThemeBtn[j].style.backgroundColor = "#0f9";
     }
     else {
       vdoThemeImage[j].style.border="";
@@ -60,13 +74,4 @@ function vdoThemeSelect(index){
   }
 }
 
-function vdoThemeImageClick(e){
-  var index = vdoThemeImage.indexOf(e.target);
-  vdoThemeSelect(index);
-}
-
-function vdoThemeBtnClick(e){
-  var index = vdoThemeBtn.indexOf(e.target);
-  vdoThemeSelect(index);
-}
 }());
