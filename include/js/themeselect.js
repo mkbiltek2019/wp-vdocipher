24d374ebbe23aa0944aa36af94ea0e81ee7d0d0e
Milan Gupta <milan.gupta.iitd@gmail.com>
2018-10-09 23:41:10 +0530

    One last refactoring. I promise

--- include/js/themeselect.js
+++ include/js/themeselect.js
@@ -2,31 +2,31 @@
 
 var vdoTheme = document.getElementById('vdo_player_theme_options');
 
-var vdoThemeImage = [
-  document.getElementById('default1-image'),
-  document.getElementById('default2-image'),
-  document.getElementById('default3-image'),
-  document.getElementById('color1-image'),
-  document.getElementById('color2-image'),
-  document.getElementById('color3-image'),
-];
-
-var vdoThemeBtn = [
-  document.getElementById('default1-btn'),
-  document.getElementById('default2-btn'),
-  document.getElementById('default3-btn'),
-  document.getElementById('color1-btn'),
-  document.getElementById('color2-btn'),
-  document.getElementById('color3-btn'),
-];
-
-var vdoThemeVal = [
-  document.getElementById('default1-id').innerHTML,
-  document.getElementById('default2-id').innerHTML,
-  document.getElementById('default3-id').innerHTML,
-  document.getElementById('color1-id').innerHTML,
-  document.getElementById('color2-id').innerHTML,
-  document.getElementById('color3-id').innerHTML,
+var vdoThemeArr = [
+  [
+    document.getElementById('default1-image'),
+    document.getElementById('default2-image'),
+    document.getElementById('default3-image'),
+    document.getElementById('color1-image'),
+    document.getElementById('color2-image'),
+    document.getElementById('color3-image'),
+  ],
+  [
+    document.getElementById('default1-btn'),
+    document.getElementById('default2-btn'),
+    document.getElementById('default3-btn'),
+    document.getElementById('color1-btn'),
+    document.getElementById('color2-btn'),
+    document.getElementById('color3-btn'),
+  ],
+  [
+    document.getElementById('default1-id').innerHTML,
+    document.getElementById('default2-id').innerHTML,
+    document.getElementById('default3-id').innerHTML,
+    document.getElementById('color1-id').innerHTML,
+    document.getElementById('color2-id').innerHTML,
+    document.getElementById('color3-id').innerHTML,
+  ],
 ];
 
 var vdoColors = [
@@ -35,43 +35,45 @@ var vdoColors = [
 '#67AF17',
 ]
 
-var vdoThemeNumber = vdoThemeVal.length;
+var vdoThemeNumber = vdoThemeArr[0].length;
 
 window.onload = function(){
-  vdoThemeSelect(vdoThemeVal.indexOf(vdoTheme.value));
+  vdoThemeSelect(vdoThemeArr[2].indexOf(vdoTheme.value));
 }
 
 for (var k = 0; k < vdoThemeNumber; k++){
-  vdoThemeImage[k].addEventListener('click', vdoThemeImageClick);
-  vdoThemeBtn[k].addEventListener('click', vdoThemeBtnClick);
+  vdoThemeArr[0][k].addEventListener('click', vdoThemeImageClick);
+  vdoThemeArr[1][k].addEventListener('click', vdoThemeBtnClick);
 }
 
 function vdoThemeImageClick(e){
-  vdoThemeSelect(vdoThemeImage.indexOf(e.target));
+  vdoThemeSelect(vdoThemeArr[0].indexOf(e.target));
 }
 
 function vdoThemeBtnClick(e){
-  vdoThemeSelect(vdoThemeBtn.indexOf(e.target));
+  vdoThemeSelect(vdoThemeArr[1].indexOf(e.target));
 }
 
 function vdoThemeSelect(index){
   for (var j = 0; j < vdoThemeNumber; j++) {
     if (j===index){
-      vdoTheme.value = vdoThemeVal[j];
-      vdoThemeImage[j].style.borderWidth="10px";
-      vdoThemeImage[j].style.borderStyle="solid";
-      vdoThemeImage[j].style.borderColor=vdoColors[j%3];
-      vdoThemeImage[j].style.width="92%";
-      vdoThemeBtn[j].innerHTML = "Selected";
-      vdoThemeBtn[j].style.backgroundColor = "#0f9";
+      vdoTheme.value = vdoThemeArr[2][j];
+      vdoThemeArr[0][j].style.borderWidth="10px";
+      vdoThemeArr[0][j].style.borderStyle="solid";
+      vdoThemeArr[0][j].style.borderColor="#4CB78C";
+      vdoThemeArr[0][j].style.width="92%";
+      vdoThemeArr[1][j].innerHTML = "Selected";
+      vdoThemeArr[1][j].style.backgroundColor = "#0f9";
     }
     else {
-      vdoThemeImage[j].style.border="";
-      vdoThemeImage[j].style.width="100%";
-      vdoThemeBtn[j].innerHTML = "Select";
-      vdoThemeBtn[j].style.backgroundColor = "#fff";
+      vdoThemeArr[0][j].style.border="";
+      vdoThemeArr[0][j].style.width="100%";
+      vdoThemeArr[1][j].innerHTML = "Select";
+      vdoThemeArr[1][j].style.backgroundColor = "#fff";
     }
   }
 }
 
 }());
+
+
