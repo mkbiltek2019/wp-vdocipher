e88d1d877378f147b9e1cdbff9aecc5dcea85837
Milan Gupta <milan.gupta.iitd@gmail.com>
2018-10-16 20:20:12 +0530

    More changes in js for showkey

--- include/js/showkey.js
+++ include/js/showkey.js
@@ -4,39 +4,50 @@
   var toggleSpan = document.getElementById('toggle_span');
 
   var currentState;
+
   window.onload = function(e){
     toggleSpanVis();
   }
+
   toggleKey.addEventListener('click', function(e){
     e.preventDefault();
-    toggleSpanVis();
     toggleVisibility(e);
   });
+
   inputField.addEventListener('keyup', function(e){
     toggleSpanVis();
   });
+
   function toggleSpanVis(){
     if (inputField.value == false) {
       toggleSpan.style.display = 'none';
-      toggleKey.setAttribute('data-protected', 'On');
-      toggleKey.innerHTML = 'Show API Secret Key';
-      inputField.type='password';
+      hideKey();
     }
     else {
       toggleSpan.style.display = 'inline-block';
     }
   }
+
   function toggleVisibility(e){
     currentState = e.target.getAttribute('data-protected');
     if (currentState == 'On'){
-      e.target.setAttribute('data-protected', 'Off');
-      e.target.innerHTML = 'Hide API Secret Key';
-      inputField.type='text';
+      showKey();
     }
     else {
-      e.target.setAttribute('data-protected', 'On');
-      e.target.innerHTML = 'Show API Secret Key';
-      inputField.type='password';
+      hideKey();
     }
   }
+
+  function showKey(){
+    toggleKey.setAttribute('data-protected', 'Off');
+    toggleKey.innerHTML = 'Hide API Secret Key';
+    inputField.type='text';
+  }
+
+  function hideKey(){
+    toggleKey.setAttribute('data-protected', 'On');
+    toggleKey.innerHTML = 'Show API Secret Key';
+    inputField.type='password';
+  }
+
 }());
