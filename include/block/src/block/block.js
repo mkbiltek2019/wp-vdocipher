d8bb6dab6f5f91a797dd21735c9d68d56f36c562
Milan Gupta <milan.gupta.iitd@gmail.com>
2018-12-13 18:27:28 +0530

    Added width and height attributes to inspector controls panel

--- include/block/src/block/block.js
+++ include/block/src/block/block.js
@@ -3,7 +3,8 @@ import './editor.scss';
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 const { Component } = wp.element;
-const { Button, TextControl } = wp.components;
+const { Button, TextControl, PanelBody, PanelRow } = wp.components;
+const { BlockControls, InspectorControls, RichText, AlignmentToolbar } = wp.editor;
 
 registerBlockType( 'vdo/block', {
   title:  __('VdoCipher Embed video'),
@@ -19,21 +20,51 @@ registerBlockType( 'vdo/block', {
     id: {
       type: 'string'
     },
+    width: {
+      type: 'string'
+    },
+    height: {
+      type: 'string'
+    }
   },
    edit: function(props) {
-    const { id } = props.attributes;
+    const { focus, setAttributes } = props;
+    const { id, height, width } = props.attributes;
     function onIDChange(id){
-      props.setAttributes({id});
+      setAttributes({id});
     }
-
+    function onHeightChange(height){
+      setAttributes({height});
+    }
+    function onWidthChange(width){
+      setAttributes({width});
+    }
     return (
       <div className={props.className}>
+        <InspectorControls>
+          <PanelBody title="Player Width and Height Settings">
+            <PanelRow>
+              <TextControl
+                label='Player Width'
+                value={ width }
+                onChange={ onWidthChange }
+              />
+            </PanelRow>
+            <PanelRow>
+              <TextControl
+                label='Player Height'
+                value={ height }
+                onChange={ onHeightChange }
+              />
+            </PanelRow>
+          </PanelBody>
+        </InspectorControls>
         <h3> VdoCipher Embed</h3>
         <span>Video ID: </span>
         <TextControl
-         id="vdo-id-input"
-         value={id}
-         onChange = {onIDChange}
+           id="vdo-id-input"
+           value={id}
+           onChange = {onIDChange}
          />
       </div>
     );
